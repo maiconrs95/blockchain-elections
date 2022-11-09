a67e4f7f76d60cc0c02c2a56c256c062d6548af5
Maicon Silva <maiconrs95@gmail.com>
2022-11-08 21:41:36 -0300

    feat: accept node server to receive application payload

--- app/blockchain-api/index.js
+++ app/blockchain-api/index.js
@@ -1,6 +1,11 @@
-var restify = require('restify');
+var restify = require("restify");
+
+const { registerBlockchain } = require("./sawtooth/client");
 
 function registerVote(req, res, next) {
+  const voto = req.body;
+  registerBlockchain(voto);
+
   res.send(200);
   next();
 }
@@ -9,8 +14,8 @@ var server = restify.createServer();
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.bodyParser());
 
-server.post('/register/vote', registerVote);
+server.post("/register/vote", registerVote);
 
-server.listen(8084, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(8084, function () {
+  console.log("%s listening at %s", server.name, server.url);
 });
